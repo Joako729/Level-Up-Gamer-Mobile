7c62a20e1a5a913d2beeed9730b6af7a37e464ba
Joaquin Caceres <jo.caceresn@duocuc.cl>
2025-11-09 06:59:04 -0300

    Solicitar permisos en tiempo de ejecucion

--- app/src/main/java/com/example/level_up/MainActivity.kt
+++ app/src/main/java/com/example/level_up/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.example.level_up
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import com.example.level_up.ui.theme.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.LaunchedEffect
 import com.example.level_up.ui.screens.LevelUpNavHost
-
+import com.example.level_up.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             LevelupTheme {
+                val permissionsToRequest = arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+                )
+
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestMultiplePermissions()
+                ) { permissions ->
+                    // Aquí puedes manejar la respuesta del usuario.
+                    // Por ejemplo, verificar si los permisos fueron concedidos o denegados.
+                }
+
+                // Solicita los permisos cuando la app se inicia
+                LaunchedEffect(Unit) {
+                    launcher.launch(permissionsToRequest)
+                }
+
                 LevelUpNavHost()
             }
         }
